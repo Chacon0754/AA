14dd619a130f9349b7e4c142ea1f7cd1ef46024a
Martín Chacón <chacon0754@outlook.com>
2024-10-23 11:24:38 -0600

    actualizado

--- P3/bubbleSort.c
+++ P3/bubbleSort.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int arr[] = {6, 7, 8, 10, 5, 4, 1, 9, 20, 89};
+int arr[] = {1, 2, 3, 4, 5, 7, 6};
 int counter;
+int exchanges;
+int comparisons;
 
 void bubbleSort();
 void showArray();
 int arraySize();
 
-
 int main(int argc, char const *argv[])
 {
+    clock_t start, end;
+    double cpuTimeUsed;
     printf("Original array:\n");
     showArray();
+    start = clock();
     bubbleSort();
+    end = clock();
+    cpuTimeUsed = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("\n\nOrdered array:\n");
     showArray();
+    printf("\nComparisons: %i. Exchanges: %i. Time used: %f", comparisons, exchanges, cpuTimeUsed);
     return 0;
 }
 
+void bubbleSort()
+{
+    exchanges, comparisons = 0;
 
-void bubbleSort(){
     int n = arraySize();
     counter = n;
-    for (int i = 0; i < n && counter != 0; i++){
+    for (int i = 0; i < n && counter != 0; i++)
+    {
+        counter = 0;
+        comparisons++;
         for (int j = 0; j <= n - 1 - i; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -32,19 +45,23 @@ void bubbleSort(){
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 counter++;
+                exchanges++;
             }
         }
     }
 }
 
-void showArray(){
+void showArray()
+{
     int n = arraySize();
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         printf("%i, ", arr[i]);
     }
 }
 
-int arraySize(){
+int arraySize()
+{
     int len = sizeof(arr) / sizeof(arr[0]);
     return len;
 }
